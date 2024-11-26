e6ebcd718621d468a50164b944ed970b7866569f
iBz-04 <issakaibrahimrayamah@gmail.com>
2024-11-26 16:13:34 +0300

    fix: update description

--- src/lib/featured-links.ts
+++ src/lib/featured-links.ts
@@ -4,7 +4,7 @@ const featuredLinks = [
 		title: "OmniBot AI",
 		position: "Founder",
 		description:
-			"Built interfaces to provide offline and private LLM and SLM services for users",
+			"Built interfaces to provide offline and private LLM & SLM services for users",
 			type: "Startup",
 			icon: "i-lucide-qr-code text-blue-500 text-lg",
 		
