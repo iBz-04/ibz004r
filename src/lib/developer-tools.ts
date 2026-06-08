4d9654dd669c66a07a42766c45500bf6ac657c9d
Ibrahim Rayamah <issakaibrahimrayamah@gmail.com>
2026-06-08 17:08:56 +0300

    update list

--- src/lib/developer-tools.ts
+++ src/lib/developer-tools.ts
@@ -1,21 +1,9 @@
 const developerTools = [
-   {
+  {
     title: 'Devseeker',
     description:
       'An autonomous llm coding agent that automates programming tasks. It generates, improves, and debugs code',
     url: 'github.com/iBz-04/DevSeeker'
-  },
-   {
-    title: 'oky studio',
-    description:
-      'An indie app studio with 5+ apps for desktop and mobile platforms on the internet',
-    url: 'okystudio.com'
-  },
-  {
-    title: 'Pymanager',
-    description:
-      'The Official Python Install Manager application (for Windows Operating System only) used by millions',
-    url: 'github.com/python/pymanager'
   },
    {
     title: 'quaynor',
@@ -24,22 +12,9 @@ const developerTools = [
     url: 'quaynor.site'
   },
   {
-    title: 'Logoslate',
-    description:
-      'A graphics design desktop application for creating quality business logos in seconds',
-    url: 'logoslate.com',
-  },
-
-  {
-    title: '5G UAV',
-    description:
-      ' Research on architecture of Emergency Communication Systems in Disasters through UAVs in 5G',
-    url: 'mdpi.com/2504-446X/7/1/25'
-  },
-  {
     title: 'Offeline',
     description:
-      'An web and desktop appl for running LLMs natively on devices with offline inference and custom memory management.',
+      'An web and desktop app for running LLMs natively on devices with offline inference and custom memory management.',
     url: 'offeline.site'
   },
   {
@@ -48,13 +23,30 @@ const developerTools = [
       'A computer use agent for windows OS, it provides automation without computer vision.',
     url: 'pypi.org/project/raya-agent'
   },
-  {
+   {
     title: 'Seeker',
     description:
       'An open source deep research AI agent, capable of iterative deep-dive investigations.',
     url: 'github.com/iBz-04/Seeker'
+  }, 
+  {
+    title: '5G uav',
+    description:
+      ' Research on architecture of Emergency Communication Systems in Disasters through UAVs in 5G',
+    url: 'mdpi.com/2504-446X/7/1/25'
+  },
+  {
+    title: 'oky studio',
+    description:
+      'An indie app studio with 5+ apps for desktop and mobile platforms on the internet',
+    url: 'okystudio.com'
+  },
+  {
+    title: 'Logoslate',
+    description:
+      'A graphics design desktop application for creating quality business logos in seconds',
+    url: 'logoslate.com',
   }
-
 ]
 
 export default developerTools
